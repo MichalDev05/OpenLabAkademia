14c8ee581f09c7557d2ea1571eba1823e851ec50
MichalDev05 <71924908+MichalDev05@users.noreply.github.com>
2023-01-16 19:12:29 +0100

    Add files via upload

---
+++ OCMS_lvl2/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Michal\Logovac\Models\Prichod;
+
+
+Route::post("api/newRecord",function(){
+
+    if (!isset($_POST["name"])){
+        die("error: name is null");
+    }
+
+
+    Prichod::create([
+        'name' => $_POST["name"]
+    ]);
+
+    return("New record " . $_POST["name"] . " was added!");
+
+});
+
+
+
+
+Route::get("api/getAllRecords",function(){
+
+
+    return Prichod::get();
+
+
+});
+
+
+
+?>
