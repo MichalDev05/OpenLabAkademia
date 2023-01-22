7661da5f5ab2eace284c1e0ba895947725688990
MichalDev05 <miskofrater@gmail.com>
2023-01-22 15:53:37 +0100

    lvl 3 review 3 (updatnuty controller)

--- plugins/michal/logger/http/controllers/LogController.php
+++ plugins/michal/logger/http/controllers/LogController.php
@@ -6,54 +6,55 @@ use Michal\Logger\Http\Resources\LogResource;
 use Rainlab\User\Facades\Auth;
 use Michal\Logger\Models\Log;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Routing\Controller;
 
 
-    class LogController{
+class LogController extends Controller{
 
-        public function getAllRecords()
-        {
-
-
-            return LogResource::collection(Log::all());
-        }
-
-        // public function loginLog()
-        // {
-        //     $user = Auth::authenticate([
-        //         'login' => post('login'),
-        //         'password' =>  post('password')
-        //     ]);
+    public function getAllRecords()
+    {
 
 
-        //     Log::create([
-        //         'name' => $user["name"],
-        //         'user_id' => $user["id"]
-        //     ]);
-
-        //     return ($user["name"]);
-        // }
-
-        public function getMyRecords()
-        {
-            Event::fire("customEvent");
-
-            return LogResource::collection(Log::where("user_id", auth()->user()->id)->get());
-
-
-        }
-
-
-
-        public function newLog(){
-
-            Log::create([
-                'name' => auth()->user()->name,
-                'user_id' => auth()->user()->id
-            ]);
-
-            return;
-
-        }
+        return LogResource::collection(Log::all());
     }
+
+    // public function loginLog()
+    // {
+    //     $user = Auth::authenticate([
+    //         'login' => post('login'),
+    //         'password' =>  post('password')
+    //     ]);
+
+
+    //     Log::create([
+    //         'name' => $user["name"],
+    //         'user_id' => $user["id"]
+    //     ]);
+
+    //     return ($user["name"]);
+    // }
+
+    public function getMyRecords()
+    {
+        Event::fire("customEvent");
+
+        return LogResource::collection(Log::where("user_id", auth()->user()->id)->get());
+
+
+    }
+
+
+
+    public function newLog(){
+
+        $log = new Log;
+        $log->user_id =auth()->user()->id;
+        $log->name =auth()->user()->name;
+        $log->save();
+        return LogResource::make($log);
+
+
+    }
+}
 
 ?>
